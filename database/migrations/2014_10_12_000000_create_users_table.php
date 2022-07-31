2265e76fbb25641d42ea4481e2f85b4819be2948
senaszel <marekstanislawmeyer@gmail.com>
2022-07-31 18:28:26 +0200

    change on isAdmin add default null to prevent not null exception while registering new user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->boolean('isAdmin');
+            $table->boolean('isAdmin')->default(false);
             $table->timestamps();
         });
     }
