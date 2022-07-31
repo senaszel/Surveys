c0cc3ea24fa578befd8d1f62a8e86b93b79181ec
senaszel <marekstanislawmeyer@gmail.com>
2022-07-31 21:14:57 +0200

    add debugbar

---
+++ resources/views/OpinionFormingUsers/index.blade.php
@@ -0,0 +1,22 @@
+<x-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Strona główna') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1>OpinionFormingUsers.index.blade.php</h1>
+                </div>
+                <div>
+                    @foreach ($opinionFormingUsers as $user)
+                        <h2><pre>{{ $user->name }} {{ $user->personals ?$user->personals->filled_surveys_count : ''}}</pre></h2>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
