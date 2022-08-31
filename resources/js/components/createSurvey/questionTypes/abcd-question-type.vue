c506ada2072b4006025b68e5c0b8f8a688aed93c
senaszel <marekstanislawmeyer@gmail.com>
2022-08-31 16:55:48 +0200

    add panels for button on sides of abcd-question-type.vue component

--- resources/js/components/createSurvey/questionTypes/abcd-question-type.vue
+++ resources/js/components/createSurvey/questionTypes/abcd-question-type.vue
@@ -1,76 +1,83 @@
 <template>
-  <div class="
-      flex flex-col flex-1
-      items-stretch
-      w-full
-      mx-auto
-      p-8
-      shadow-lg
-      rounded-xl
-      bg-red-50
-      hover:bg-slate-300 hover:text-black
-    ">
-    <button @click.prevent="$emit('deleteElement', id)"
-      class="p-2 text-xl font-bold text-red-50 hover:text-red-500 place-self-end flex-grow-0">delete</button>
-    <div class="flex flex-row flex-1 items-stretch w-full p-4">
-      <label class="w-[23%] mr-[2%] font-bold" :for="questionABCD">Wpisz treść pytania</label>
-      <input class="w-[75%]" type="text" :name="questionABCD" :id="questionABCDId" placeholder="wpisz treść pytania" />
+  <div class="flex flex-row flex-1">
+    <div class="absolute right-[80%]">
+      <div class="flex flex-col flex-1 w-full mx-auto p-8 shadow-lg rounded-xl bg-red-50 hover:bg-slate-300 hover:text-black space-y-28">
+        <button @click.prevent="" class="justify-self-start bg-blue-400 hover:bg-blue-600 hover:text-white p-4 rounded-full">przesuń w górę</button>
+        <button @click.prevent="" class="justify-self-end bg-blue-400 hover:bg-blue-600 hover:text-white p-4 rounded-full">przesuń w dół</button>
+      </div>
     </div>
-    <div class="flex flex-row flex-1 items-stretch w-full p-4">
-      <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerA">Wpisz treść mozliwej
-        odpowiedzi</label>
-      <input class="w-[75%]" type="text" :name="questionABCD_answerA" :id="answerA"
-        placeholder="   wpisz treść mozliwej odpowiedzi A"/>
+
+    <div class="flex flex-col flex-1 items-stretch w-full mx-auto p-8 shadow-lg rounded-xl bg-red-50 hover:bg-slate-300 hover:text-black">
+      <div class="flex flex-row flex-1 items-stretch w-full p-4">
+        <label class="w-[23%] mr-[2%] font-bold" :for="questionABCD">Wpisz treść pytania</label>
+        <input class="w-[75%]" type="text" :name="questionABCD" :id="questionABCDId"
+          placeholder="wpisz treść pytania" />
+      </div>
+      <div class="flex flex-row flex-1 items-stretch w-full p-4">
+        <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerA">Wpisz treść mozliwej
+          odpowiedzi</label>
+        <input class="w-[75%]" type="text" :name="questionABCD_answerA" :id="answerA"
+          placeholder="   wpisz treść mozliwej odpowiedzi A" />
+      </div>
+      <div class="flex flex-row flex-1 items-stretch w-full p-4">
+        <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerB">Wpisz treść mozliwej
+          odpowiedzi</label>
+        <input class="w-[75%]" type="text" :name="questionABCD_answerB" :id="answerB"
+          placeholder="   wpisz treść mozliwej odpowiedzi B" />
+      </div>
+      <div class="flex flex-row flex-1 items-stretch w-full p-4">
+        <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerC">Wpisz treść mozliwej
+          odpowiedzi</label>
+        <input class="w-[75%]" type="text" :name="questionABCD_answerC" :id="answerC"
+          placeholder="   wpisz treść mozliwej odpowiedzi C" />
+      </div>
+      <div class="flex flex-row flex-1 items-stretch w-full p-4">
+        <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerD">Wpisz treść mozliwej
+          odpowiedzi</label>
+        <input class="w-[75%]" type="text" :name="questionABCD_answerD" :id="answerD"
+          placeholder="   wpisz treść mozliwej odpowiedzi D" />
+      </div>
     </div>
-    <div class="flex flex-row flex-1 items-stretch w-full p-4">
-      <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerB">Wpisz treść mozliwej
-        odpowiedzi</label>
-      <input class="w-[75%]" type="text" :name="questionABCD_answerB" :id="answerB"
-        placeholder="   wpisz treść mozliwej odpowiedzi B"/>
+   
+    <div class="absolute left-[80%]">
+      <div class="flex flex-col flex-1 items-stretch w-full mx-auto p-8 shadow-lg rounded-xl bg-red-50 hover:bg-slate-300 hover:text-black space-y-8">
+        <button @click.prevent="$emit('deleteElement', id)"
+        class="p-4 rounded-full bg-blue-400 text-xl font-bold hover:bg-red-500">delete</button>
+        <button @click.prevent="" class="bg-blue-400 p-4 rounded-full">dłuszy tekst w buttonie</button>
+        <button @click.prevent="" class="bg-blue-400 p-4 rounded-full">raz</button>
     </div>
-    <div class="flex flex-row flex-1 items-stretch w-full p-4">
-      <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerC">Wpisz treść mozliwej
-        odpowiedzi</label>
-      <input class="w-[75%]" type="text" :name="questionABCD_answerC" :id="answerC"
-        placeholder="   wpisz treść mozliwej odpowiedzi C" />
-    </div>
-    <div class="flex flex-row flex-1 items-stretch w-full p-4">
-      <label class="self-center w-[23%] mr-[2%] font-bold" :for="answerD">Wpisz treść mozliwej
-        odpowiedzi</label>
-      <input class="w-[75%]" type="text" :name="questionABCD_answerD" :id="answerD"
-        placeholder="   wpisz treść mozliwej odpowiedzi D" />
-    </div>
+  </div>
   </div>
 </template>
   
   <script>
-  export default {
+export default {
 
-    props: {
-      id: { required: true, type: String },
-      type: { required: true, type: String },
-    },
+  props: {
+    id: { required: true, type: String },
+    type: { required: true, type: String },
+  },
 
-    data() {
-      return {
-        questionABCD: 'questions[' + this.id + '][abcd][question][name]',
-        questionABCDId: 'questions-' + this.id + '-question-abcd',
-        questionABCD_answerA: 'questions[' + this.id + '][abcd][answers][a]',
-        answerA: 'questions-' + this.id + '-answer-a',
-        questionABCD_answerB: 'questions[' + this.id + '][abcd][answers][b]',
-        answerB: 'questions-' + this.id + '-answer-b',
-        questionABCD_answerC: 'questions[' + this.id + '][abcd][answers][c]',
-        answerC: 'questions-' + this.id + '-answer-c',
-        questionABCD_answerD: 'questions[' + this.id + '][abcd][answers][d]',
-        answerD: 'questions-' + this.id + '-answer-d',
-      };
-    },
+  data() {
+    return {
+      questionABCD: 'questions[' + this.id + '][abcd][question][name]',
+      questionABCDId: 'questions-' + this.id + '-question-abcd',
+      questionABCD_answerA: 'questions[' + this.id + '][abcd][answers][a]',
+      answerA: 'questions-' + this.id + '-answer-a',
+      questionABCD_answerB: 'questions[' + this.id + '][abcd][answers][b]',
+      answerB: 'questions-' + this.id + '-answer-b',
+      questionABCD_answerC: 'questions[' + this.id + '][abcd][answers][c]',
+      answerC: 'questions-' + this.id + '-answer-c',
+      questionABCD_answerD: 'questions[' + this.id + '][abcd][answers][d]',
+      answerD: 'questions-' + this.id + '-answer-d',
+    };
+  },
 
-    methods: {
-      //methods
-    },
-  };
-  </script>
+  methods: {
+    //methods
+  },
+};
+</script>
   
   <style>
   </style>
