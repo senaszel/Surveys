826096cafaae54ab5064f60e3326a5766e967aab
aasky <skya82312@gmail.com>
2022-09-27 21:20:34 +0200

    add image upload

--- resources/views/create-survey.blade.php
+++ resources/views/create-survey.blade.php
@@ -1,5 +1,9 @@
 <x-layout>
-
+    <div>
+        <button>
+            <a href="{{ route('upload') }}">Dodaj zdjęcie</a>
+        </button>
+    </div>
     <div id="vue-create-survey">@csrf</div>
 
 </x-layout>
